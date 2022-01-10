02a2cb5c4054a22b757c9b866811dfc72c945fff
alvarobgodoy <alvaro.godoy2002@gmail.com>
2022-01-10 09:47:30 -0300

    Created tictactoe.py file

---
+++ tictactoe.py
@@ -0,0 +1,10 @@
+'''CSE 210 - Programming with Classes
+Author: Alvaro B. Godoy
+Assignment: https://byui-cse.github.io/cse210-course-competency/introduction/materials/tictactoe-specification.html
+'''
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
